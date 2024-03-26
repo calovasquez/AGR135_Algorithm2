dca91c9f3217354ac4acaa8962f21abccd0e5056
Carlos Vásquez <carlos.vasquez.sc@gmail.com>
2024-03-25 23:33:42 -0600

    Added algorithm2.h

---
+++ algorithm2.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   algorithm2.h
+ * Author: vasquez
+ *
+ * Created on March 25, 2024, 11:32 PM
+ */
+
+#ifndef ALGORITHM2_H
+#define ALGORITHM2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ALGORITHM2_H */
